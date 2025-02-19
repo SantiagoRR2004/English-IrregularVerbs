2eb986eec7291ad3f71fe6b94deb6b42309f9944
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-02-19 08:27:17 +0100

    Made into class

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 import tkinter as tk
 
 
-numOfVerbs = 30
+class ExamCreator:
+
+    NUMBEROFVERBS = 30
+
+    def __init__(self) -> None:
+        self.reset()
+        self.window.mainloop()
+
+    def reset(self):
+        self.numOfVerbs = self.NUMBEROFVERBS
+        self.createWindow()
+
+    def createWindow(self):
+        self.window = tk.Tk()
+        self.window.title("Irregular Verb Exam Creator")
+        self.window.geometry("800x600")
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.createLabel()
+        self.createSpinbox()
+        self.createButton()
+
+    def createLabel(self):
+        label = tk.Label(self.window, text="Number of verbs:")
+        label.grid(row=0, column=0, padx=5, pady=10, sticky="w")
+
+    def createSpinbox(self):
+        self.spinbox = tk.Spinbox(
+            self.window,
+            from_=1,
+            to=float("inf"),
+            width=5,
+            textvariable=tk.StringVar(value=self.numOfVerbs),
+        )
+        self.spinbox.grid(row=0, column=1, padx=5, pady=10, sticky="w")
+
+    def createButton(self):
+        button = tk.Button(self.window, text="Create exams", command=self.save_number)
+        button.place(
+            relx=1.0, rely=1.0, anchor="se", x=-10, y=-10
+        )  # Places it in the bottom-right with some padding
+
+    def save_number(self):
+        global numOfVerbs
+        try:
+            self.numOfVerbs = int(self.spinbox.get())  # Convert input to an integer
+        except ValueError:
+            pass
 
 
-def save_number():
-    global numOfVerbs
-    try:
-        numOfVerbs = int(spinbox.get())  # Convert input to an integer
-    except ValueError:
-        pass
-
-
-root = tk.Tk()
-root.title("Irregular Verb Exam Creator")
-root.geometry("800x600")  # Set width to 800 and height to 600
-
-# Label
-label = tk.Label(root, text="Number of verbs:")
-label.grid(row=0, column=0, padx=5, pady=10, sticky="w")
-
-# Spinbox with arrows for increasing/decreasing the number
-spinbox = tk.Spinbox(
-    root, from_=1, to=float("inf"), width=5, textvariable=tk.StringVar(value=numOfVerbs)
-)
-spinbox.grid(row=0, column=1, padx=5, pady=10, sticky="w")
-
-
-# Button to create the exams
-button = tk.Button(root, text="Create exams", command=save_number)
-button.place(
-    relx=1.0, rely=1.0, anchor="se", x=-10, y=-10
-)  # Places it in the bottom-right with some padding
-
-
-root.mainloop()
-
-
-print(numOfVerbs)
+if __name__ == "__main__":
+    ExamCreator()
